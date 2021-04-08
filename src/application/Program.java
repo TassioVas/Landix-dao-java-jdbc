b8efa2df9bbaf031544abe6d4e886ad5655014ea
tassioVas <t.santos.vasconcelos@gmail.com>
2021-04-08 11:23:04 -0300

    Classe Clientes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import model.entities.Clientes;
 import model.entities.Vendedores;
 
 public class Program {
@@ -8,7 +9,12 @@ public class Program {
 		// TODO Auto-generated method stub
 		
 		Vendedores obj = new Vendedores(2, "joao", 5, null);
+		
+		Clientes clientes = new Clientes(2, "bob", 2, 1, 2000.0);
+		
 		System.out.println(obj);
+		System.out.println(clientes);
+		
 
 	}
 
---
+++ src/model/entities/Clientes.java
@@ -0,0 +1,110 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Clientes implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer cdcl;
+	private String dsnome;
+	private Integer idtipo;
+	private Integer cdvend;
+	private Double dslim;
+	
+	private Vendedores vendedores;
+		
+	private Clientes() {
+	}
+
+	public Clientes(Integer cdcl, String dsnome, Integer idtipo, Integer cdvend, Double dslim) {
+		super();
+		this.cdcl = cdcl;
+		this.dsnome = dsnome;
+		this.idtipo = idtipo;
+		this.cdvend = cdvend;
+		this.dslim = dslim;
+	}
+
+	public Integer getCdcl() {
+		return cdcl;
+	}
+
+	public void setCdcl(Integer cdcl) {
+		this.cdcl = cdcl;
+	}
+
+	public String getDsnome() {
+		return dsnome;
+	}
+
+	public void setDsnome(String dsnome) {
+		this.dsnome = dsnome;
+	}
+
+	public Integer getIdtipo() {
+		return idtipo;
+	}
+
+	public void setIdtipo(Integer idtipo) {
+		this.idtipo = idtipo;
+	}
+
+	public Integer getCdvend() {
+		return cdvend;
+	}
+
+	public void setCdvend(Integer cdvend) {
+		this.cdvend = cdvend;
+	}
+
+	public Double getDslim() {
+		return dslim;
+	}
+
+	public void setDslim(Double dslim) {
+		this.dslim = dslim;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cdcl == null) ? 0 : cdcl.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Clientes other = (Clientes) obj;
+		if (cdcl == null) {
+			if (other.cdcl != null)
+				return false;
+		} else if (!cdcl.equals(other.cdcl))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Clientes [cdcl=" + cdcl + ", dsnome=" + dsnome + ", idtipo=" + idtipo + ", cdvend=" + cdvend
+				+ ", dslim=" + dslim + "]";
+	}
+
+	public Vendedores getVendedores() {
+		return vendedores;
+	}
+
+	public void setVendedores(Vendedores vendedores) {
+		this.vendedores = vendedores;
+	}
+	
+	
+
+}
