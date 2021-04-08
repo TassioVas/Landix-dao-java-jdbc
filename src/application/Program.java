4492d03f13a6d1e5621b60b65ce1d7fcb4b13ef5
tassioVas <t.santos.vasconcelos@gmail.com>
2021-04-08 11:02:21 -0300

    Classe Vendedores

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Vendedores;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Vendedores obj = new Vendedores(2, "joao", 5, null);
+		System.out.println(obj);
+
+	}
+
+}
---
+++ src/model/entities/Vendedores.java
@@ -0,0 +1,88 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Vendedores implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer codvend;
+	private String dsnome;
+	private Integer cdtab;
+	private Date dtnasc;
+	
+	public Vendedores() {
+		
+	}
+
+	public Vendedores(Integer codvend, String dsnome, Integer cdtab, Date dtnasc) {
+		this.codvend = codvend;
+		this.dsnome = dsnome;
+		this.cdtab = cdtab;
+		this.dtnasc = dtnasc;
+	}
+
+	public Integer getcodvend() {
+		return codvend;
+	}
+
+	public void setcodvend(Integer codvend) {
+		this.codvend = codvend;
+	}
+
+	public String getdsnome() {
+		return dsnome;
+	}
+
+	public void setdsnome(String dsnome) {
+		this.dsnome = dsnome;
+	}
+
+	public Integer getcdtab() {
+		return cdtab;
+	}
+
+	public void setcdtab(Integer cdtab) {
+		this.cdtab = cdtab;
+	}
+
+	public Date getdtnasc() {
+		return dtnasc;
+	}
+
+	public void setdtnasc(Date dtnasc) {
+		this.dtnasc = dtnasc;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codvend == null) ? 0 : codvend.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vendedores other = (Vendedores) obj;
+		if (codvend == null) {
+			if (other.codvend != null)
+				return false;
+		} else if (!codvend.equals(other.codvend))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "vendedores [codvend=" + codvend + ", dsnome=" + dsnome + ", cdtab=" + cdtab + ", dtnasc=" + dtnasc
+				+ "]";
+	}
+}
